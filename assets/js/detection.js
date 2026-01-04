dac81fcf83042fb1c08ff93be8f3d2c5ee642460
Inggih Wicaksono <igihcksn@gmail.com>
2026-01-04 18:01:13 +0700

    feat: update todo format & starter project

---
+++ assets/js/detection.js
@@ -0,0 +1,35 @@
+class ObjectDetector {
+    constructor() {
+        this.model = null;
+        this.labels = [];
+    }
+
+    /**
+     * TODO:
+     * Lengkapi metode untuk memuat model:
+     * [] Muat model dari './model/model.json'
+     * [] Muat metadata dari './model/metadata.json'
+    */
+    async loadModel() {}
+
+    /**
+     * TODO:
+     * Lengkapi metode untuk prediksi:
+     * [] Logika preprosesing gambar
+     * [] Lakukan prediksi menggunakan model yang dimuat
+     * [] Kembalikan hasil prediksi dengan className dan confidence
+    */
+    async predict(imageElement) {}
+
+    isLoaded() {
+        return !!this.model;
+    }
+
+    /**
+     * TODO:
+     * [] Lengkapi metode untuk menonaktifkan model menggunakan dispose
+    */
+    dispose() {}
+}
+
+export default ObjectDetector;
