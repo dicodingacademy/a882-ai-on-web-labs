3f084c528ff982d22cca4b73c2a643a371cf5e1c
Inggih Wicaksono <igihcksn@gmail.com>
2026-01-12 17:27:55 +0700

    fix: indentation & autoplay video.

--- assets/js/detection.js
+++ assets/js/detection.js
@@ -1,78 +1,78 @@
 class ObjectDetector {
-    constructor() {
-        this.model = null;
-        this.labels = [];
-    }
+	constructor() {
+		this.model = null;
+		this.labels = [];
+	}
 
-    /**
-     * TODO:
-     * Lengkapi metode untuk memuat model:
-     * [✓] Muat model dari './model/model.json'
-     * [✓] Muat metadata dari './model/metadata.json'
-    */
-    async loadModel() {
-        try {
-            const [metadata, model] = await Promise.all([
-                fetch('./model/metadata.json').then(r => r.json()),
-                tf.loadLayersModel('./model/model.json')
-            ]);
+	/**
+	 * TODO:
+	 * Lengkapi metode untuk memuat model:
+	 * [✓] Muat model dari './model/model.json'
+	 * [✓] Muat metadata dari './model/metadata.json'
+	*/
+	async loadModel() {
+		try {
+			const [metadata, model] = await Promise.all([
+				fetch('./model/metadata.json').then(r => r.json()),
+				tf.loadLayersModel('./model/model.json')
+			]);
 
-            this.labels = metadata.labels;
-            this.model = model;
+			this.labels = metadata.labels;
+			this.model = model;
 
-            return { success: true, labels: this.labels };
-        } catch (error) {
-            throw new Error('Gagal memuat model. Periksa file model.');
-        }
-    }
+			return { success: true, labels: this.labels };
+		} catch (error) {
+			throw new Error('Gagal memuat model. Periksa file model.');
+		}
+	}
 
-    /**
-     * TODO:
-     * Lengkapi metode untuk prediksi:
-     * [✓] Logika preprosesing gambar
-     * [✓] Lakukan prediksi menggunakan model yang dimuat
-     * [✓] Kembalikan hasil prediksi dengan className dan confidence
-    */
-    async predict(imageElement) {
-        if (!this.model) throw new Error('Model tidak dapat dimuat');
+	/**
+	 * TODO:
+	 * Lengkapi metode untuk prediksi:
+	 * [✓] Logika preprosesing gambar
+	 * [✓] Lakukan prediksi menggunakan model yang dimuat
+	 * [✓] Kembalikan hasil prediksi dengan className dan confidence
+	*/
+	async predict(imageElement) {
+		if (!this.model) throw new Error('Model tidak dapat dimuat');
 
-        const tensor = tf.tidy(() => 
-            tf.browser.fromPixels(imageElement)
-                .resizeBilinear([224, 224])
-                .div(255.0)
-                .expandDims(0)
-        );
+		const tensor = tf.tidy(() =>
+			tf.browser.fromPixels(imageElement)
+				.resizeBilinear([224, 224])
+				.div(255.0)
+				.expandDims(0)
+		);
 
-        const predictions = this.model.predict(tensor);
-        const values = await predictions.data();
-        
-        const maxIndex = values.indexOf(Math.max(...values));
-        const result = {
-            className: this.labels[maxIndex],
-            confidence: Math.round(values[maxIndex] * 100)
-        };
+		const predictions = this.model.predict(tensor);
+		const values = await predictions.data();
 
-        // Cleanup
-        tensor.dispose();
-        predictions.dispose();
+		const maxIndex = values.indexOf(Math.max(...values));
+		const result = {
+			className: this.labels[maxIndex],
+			confidence: Math.round(values[maxIndex] * 100)
+		};
 
-        return result;
-    }
+		// Cleanup
+		tensor.dispose();
+		predictions.dispose();
 
-    isLoaded() {
-        return !!this.model;
-    }
+		return result;
+	}
 
-    /**
-     * TODO:
-     * [✓] Lengkapi metode untuk menonaktifkan model menggunakan dispose
-    */
-    dispose() {
-        if (this.model) {
-            this.model.dispose();
-            this.model = null;
-        }
-    }
+	isLoaded() {
+		return !!this.model;
+	}
+
+	/**
+	 * TODO:
+	 * [✓] Lengkapi metode untuk menonaktifkan model menggunakan dispose
+	*/
+	dispose() {
+		if (this.model) {
+			this.model.dispose();
+			this.model = null;
+		}
+	}
 }
 
 export default ObjectDetector;
