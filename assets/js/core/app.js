42df7ddb3d396cbab077c23b877d6523a6073437
Inggih Wicaksono <igihcksn@gmail.com>
2026-02-11 12:05:55 +0700

    Pass UI to NutritionService constructor

--- assets/js/core/app.js
+++ assets/js/core/app.js
@@ -57,7 +57,7 @@ class NutriApp {
 
       this.camera = new CameraService();
 
-      this.generator = new NutritionService();
+      this.generator = new NutritionService(this.ui);
 
       try {
           await this.generator.loadModel();
